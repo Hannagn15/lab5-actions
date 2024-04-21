bee36d23b5951d16d6e4f4d6b6e466599610a33d
hannagn15 <hannagn15@hotmail.com>
2024-04-21 15:23:26 +0200

    pueba de cicd para operaciones matematicas

--- suma.py
+++ suma.py
@@ -4,3 +4,29 @@ def sumar(a,b):
 
 if __name__=="__main__":
     print(sumar(5,3))
+
+
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print(restar(5, 3))
+
+
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(multiplicar(5, 3))
+
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero"
+    else:
+        return a / b
+
+if __name__ == "__main__":
+    print(dividir(5, 3))
+
+
--- test_suma.py
+++ test_suma.py
@@ -1,8 +1,12 @@
 import unittest
 from suma import sumar
+from suma import restar
+from suma import multiplicar
+from suma import dividir
 
 
-class testSymar(unittest.TestCase):
+
+class testSumar(unittest.TestCase):
 
 
     def test_sumar(self):
@@ -13,3 +17,38 @@ class testSymar(unittest.TestCase):
 
 if __name__== '__main__':
     unittest.main()
+
+
+class TestRestar(unittest.TestCase):
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+import unittest
+
+class TestMultiplicar(unittest.TestCase):
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+import unittest
+
+class TestDividir(unittest.TestCase):
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 2), 3)
+        self.assertEqual(dividir(-4, 2), -2)
+        self.assertEqual(dividir(5, 0), "Error: No se puede dividir por cero")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
